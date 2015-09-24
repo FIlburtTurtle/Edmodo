acaf62201ab195cfcd836e6e76b002fef627106b
FIlburtTurtle <rancheta@berkeley.edu>
2015-09-24 15:37:13 -0700

    started edmodo client wrapper

--- public/js/edmodo_angular.js
+++ public/js/edmodo_angular.js
@@ -1,4 +1,21 @@
 // API wrapper to get edmodo results in angular
-var EdmodoAngular = angular.module('EdmodoAngular', []);
+var EdmodoAngular = angular.module('EdmodoAngular', [])
 
-EdmodoAngular.factory
+EdmodoAngular.factory('api', ['$http', function($http){
+
+	function api(client_id, client_secret){
+		this.client_id = client_id;
+		this.client_secret = client_secret
+	}
+
+	api.prototype.configure = function(client_id, client_secret)
+	{
+
+	}
+
+	api.prototype.getAssignments = function()
+	{
+
+	}
+
+}])
