f8b3720226e73881fb668dfd834dbce5aaf36855
FIlburtTurtle <rancheta@berkeley.edu>
2015-09-24 19:31:24 -0700

    finished list panel directive

--- public/js/directives.js
+++ public/js/directives.js
@@ -1,11 +1,17 @@
-//INDEX PAGE CONTROLLER
-edmodo.directive('listpanel', function(){
+//list panel directive
+edmodo.directive('listpanel',['$window', function($window){
 	return {
 		restrict : 'E',
 		scope : {
 			list : '=',
-			title : '='
+			paneltitle : '='
 		},
-		templateUrl : '/partials/listPanel.html'
+		templateUrl : '/partials/listPanel.html',
+		link: function($scope, $element, attr){
+			var sidebar = angular.element($element.children()[1]);
+			var max_height = $window.innerHeight * Number(attr.maxPercent || 0.85);
+			angular.element(sidebar).css('max-height', max_height + 'px')
+		}
 	}
-});
+}]);
+
